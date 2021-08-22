49682dbaa38c722144eef396e40c6aeb8ce3db97
lcmurillor <lcmurillor@est.utn.ac.cr>
2021-08-21 21:08:46 -0600

    Examen compelto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, 
+{ path: 'personajes', loadChildren: () => import('./pages/personajes/personajes.module').then(m => m.PersonajesModule) }, 
+{ path: 'episodios', loadChildren: () => import('./pages/episodios/episodios.module').then(m => m.EpisodiosModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
